1a80e5a6d5e75568a7129d34a0dcba82516925b3
Enzo Magnier <enzofegz@gmail.com>
2024-07-22 14:02:02 +0200

    python cours

---
+++ crypto/triple des/triple_des.py
@@ -0,0 +1,10 @@
+import sys
+import cryptography.hazmat.primitives.asymmetric.padding as padding
+
+
+i = 0
+
+class CryptoAnalys:
+    def __init__(self):
+        print("CryptoAnalys initialized")
+
